3b43a1b7082c44345c78c2047443331242624b11
sishoo <112181983+sishoo@users.noreply.github.com>
2024-01-18 15:48:02 -0500

    Add files via upload

---
+++ heph/types/vertex.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+
+
+
+typedef struct Vertex {
+    float x, y, z;
+
+    Vertex operator+(Vertex const& addend)
+    {
+        Vertex sum;
+        sum.x = x + addend.x;
+        sum.y = y + addend.y;
+        sum.z = z + addend.z;
+        return sum;
+    }
+
+    void operator+=(Vertex const& addend)
+    {
+        x += addend.x;
+        y += addend.y;
+        z += addend.z;
+    }
+
+    Vertex operator-(Vertex const& subtrahend)
+    {
+        Vertex difference;
+        difference.x = x - subtrahend.x;
+        difference.y = y - subtrahend.y;
+        difference.z = z - subtrahend.z;
+        return difference;
+    }
+
+    void operator-=(Vertex const& subtrahend)
+    {
+        x -= subtrahend.x;
+        y -= subtrahend.y;
+        z -= subtrahend.z;
+    }
+        
+} Vertex;
+
+
