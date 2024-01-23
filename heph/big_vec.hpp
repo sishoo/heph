1bfb3d9efe21980ff00b47628f57582a43c7df47
sishoo <112181983+sishoo@users.noreply.github.com>
2024-01-22 20:18:57 -0500

    Add files via upload

---
+++ heph/big_vec.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+
+
+
+/*
+
+    use something like virtual alloc to instead of having to cop yover all the memory on a new allocation you just commit the addressi to the page table
+
+*/
+
+
+
+template<typename T>
+class BigVector
+{
+public:
+    BigVector(size_t size)
+    {
+
+    }
+
+    void reserve()
+    {
+        
+    }
+
+};
