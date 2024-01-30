3c404511b358409dd1d45d0741994c5d311a067f
sishoo <112181983+sishoo@users.noreply.github.com>
2024-01-30 16:13:32 -0500

    Add files via upload

---
+++ heph/types/vertex.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+
+struct Vertex {
+    float x, y, z;
+};
+
+
+
+
+// template<typename T>
+// class VertexT {
+// public:
+//     T x, y, z;
+//     Vertex(T x, T y, T z) : x (x), y(y), z(z) {}
+// };
+
+
