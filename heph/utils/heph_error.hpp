45cb7116cb369469647f46ebf469c5001367464f
sishoo <112181983+sishoo@users.noreply.github.com>
2024-02-14 15:39:26 -0500

    Add files via upload

---
+++ heph/utils/heph_error.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "./heph_defines.hpp"
+#include <iostream>
+
+#define INFO_STRING " File: " << __FILE__ << " Line: " << __LINE__
+
+
+#if HEPH_VALIDATE
+    #define HEPH_PRINT_ERROR(error_msg) std::cerr <<  "Hephaestus non fatal (but noticable) error: " << error_msg << " @ "  << INFO_STRING << std::endl;
+#else 
+    #define HEPH_PRINT_ERROR(error_msg)
+#endif
+
+  /* ALWAYS WRAP THIS IN CURLY BRACES, IT EXPLANDS TO 2 LINES */
+#define HEPH_THROW_FATAL_ERROR(err_msg) std::cerr << "Hephaestus Fatal Error: " << err_msg << " @ " << INFO_STRING << std::endl; abort();
+
+#if HEPH_VALIDATE
+    #define NOTE(msg) std::cout << "Note to self: " << msg << std::endl;
+#else
+    #define NOTE(msg) 
+#endif
+
+enum HephResult
+{
+    Failure,
+    Success
+};
